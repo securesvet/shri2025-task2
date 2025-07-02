297f708cd932ead50e816909bb1b2bdb4f8aa354
Sviatoslav Murzin <mursvyats@gmail.com>
2025-07-02 22:01:05 +0500

    fix: added Footer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { realtimeDiff } from 'vite-plugin-realtime-diff'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), realtimeDiff("https://securesvet.github.io/shri2025-task")],
   base: "/shri2025-task2"
 })
